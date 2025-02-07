17b55376d8d74168b8e1f3d34fcfbf30db4fe3e3
caiovalverde20 <caiovalverde20@gmail.com>
2025-02-07 13:16:06 -0300

    Adicionado carregamento de produtos a partir de JSON em caso de BD vazio

--- ofertas/views.py
+++ ofertas/views.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from django.http import JsonResponse
 from .scraper import buscar_ofertas
 from .models import Produto
@@ -43,9 +45,30 @@ def listar_maior_desconto(request):
 def exibir_produtos(request):
     produtos = Produto.objects.all()
 
+    if not produtos.exists():
+        caminho_arquivo = "./ofertas/produtos.json"
+
+        with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+            json_produtos = json.load(arquivo)
+
+        for produto_data in json_produtos["produtos"]:
+            Produto.objects.create(
+                nome=produto_data["nome"],
+                preco=float(produto_data["preco"]),
+                imagem=produto_data["imagem"],
+                link=produto_data["link"],
+                parcelamento=produto_data["parcelamento"],
+                preco_sem_desconto=float(produto_data["preco_sem_desconto"]) if produto_data.get("preco_sem_desconto") else None,
+                percentual_desconto=float(produto_data["percentual_desconto"]) if produto_data.get("percentual_desconto") else None,
+                tipo_entrega=produto_data["tipo_entrega"],
+                frete_gratis=produto_data["frete_gratis"],
+            )
+
+        produtos = Produto.objects.all()
+
     produto_maior_preco = produtos.order_by('-preco').first()
     produto_menor_preco = produtos.order_by('preco').first()
-
+    
     produtos_com_desconto = produtos.exclude(percentual_desconto__isnull=True).exclude(percentual_desconto=0)
 
     produto_maior_desconto = produtos_com_desconto.order_by('-percentual_desconto').first()
@@ -54,5 +77,5 @@ def exibir_produtos(request):
         "produtos": produtos,
         "produto_maior_preco": produto_maior_preco,
         "produto_menor_preco": produto_menor_preco,
-        "produto_maior_desconto": produto_maior_desconto
+        "produto_maior_desconto": produto_maior_desconto,
     })

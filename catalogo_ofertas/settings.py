3d604b0e6e0c7586235f0c412fc3e11ca8b3fccd
caiovalverde20 <caiovalverde20@gmail.com>
2025-02-07 15:14:31 -0300

    Corrigido allowed_hosts

--- catalogo_ofertas/settings.py
+++ catalogo_ofertas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ey!pxt3keb%u-g%)(gp+)#e#a2@q5b03d=!ai#5ff!51f)wf1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

6368a323123810afe54bbae0498bf700370ee53d
caiovalverde20 <caiovalverde20@gmail.com>
2025-02-07 02:15:18 -0300

    Adicionado get menor preco

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('atualizar/', views.atualizar_produtos, name='atualizar_produtos'),
     path('listar/', views.listar_produtos, name='listar_produtos'),
     path('listar/maior-preco/', views.listar_maior_preco, name='listar_maior_preco'),
+    path('listar/menor-preco/', views.listar_menor_preco, name='listar_menor_preco'),
 ]
--- ofertas/views.py
+++ ofertas/views.py
@@ -32,6 +32,10 @@ def listar_maior_preco(request):
     produtos = Produto.objects.order_by('-preco')
     return render(request, "ofertas/produtos.html", {"produtos": produtos})
 
+def listar_menor_preco(request):
+    produtos = Produto.objects.order_by('preco')
+    return render(request, "ofertas/produtos.html", {"produtos": produtos})
+
 def exibir_produtos(request):
     produtos = Produto.objects.all()
     return render(request, "ofertas/produtos.html", {"produtos": produtos})

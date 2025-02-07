cd338b7bfa3ae4c70980980624736b6e7ec56fda
caiovalverde20 <caiovalverde20@gmail.com>
2025-02-06 23:07:51 -0300

    Adicionado rota get produtos

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('atualizar/', views.atualizar_produtos, name='atualizar_produtos'),
+    path('listar/', views.listar_produtos, name='listar_produtos'),
 ]
--- ofertas/views.py
+++ ofertas/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from .scraper import buscar_ofertas
+from .models import Produto
 
 def atualizar_produtos(request):
     try:
@@ -7,3 +8,16 @@ def atualizar_produtos(request):
         return JsonResponse({"mensagem": "Produtos atualizados com sucesso!"}, status=200)
     except Exception as e:
         return JsonResponse({"mensagem": "Erro ao atualizar produtos", "erro": str(e)}, status=500)
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    produtos_serializados = [
+        {
+            "nome": produto.nome,
+            "preco": produto.preco,
+            "imagem": produto.imagem,
+            "link": produto.link,
+        }
+        for produto in produtos
+    ]
+    return JsonResponse({"produtos": produtos_serializados})
